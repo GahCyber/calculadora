f7806074f0cfac6ac9a7b16732effac4db827c72
Gabriel Gedolin <155395516+GahCyber@users.noreply.github.com>
2025-02-26 10:08:48 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import "../styles/_styles.scss";
+import React, { useState } from 'react';
 
 export default function App() {
-  const [input, setInput] = useState(""); // Armazena a expressão digitada
+  const [input, setInput] = useState(''); // Armazena a expressão digitada
 
   // Função para lidar com a entrada do usuário
   const handleInput = (value) => {
@@ -12,55 +11,139 @@ export default function App() {
   // Função para calcular o resultado
   const handleCalculate = () => {
     try {
-      // Avalia a expressão usando a função eval
-      const result = eval(input);
+      // Substitui 'log' por 'Math.log' para calcular logaritmos
+      const result = eval(input.replace(/log/g, 'Math.log'));
       setInput(result.toString());
     } catch (error) {
-      setInput("Erro");
+      setInput('Erro');
     }
   };
 
   // Função para limpar o input
   const handleClear = () => {
-    setInput("");
+    setInput('');
   };
 
   // Função para capturar a tecla Enter e calcular o resultado
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       handleCalculate();
     }
   };
 
   return (
-    <div id="all">
-      <div id="campo-numero">
-        <input
-          id="input-conta"
-          type="text"
-          className="form-control"
-          placeholder="0"
-          value={input}
-          onChange={(evento) => setInput(evento.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-      </div>
-      <hr id="linha" />
-      <div id="nmb-C">
-        <button id="C" onClick={handleClear}>
-          C
-        </button>
-        {[9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map((num) => (
-          <button key={num} onClick={() => handleInput(num.toString())}>
-            {num}
-          </button>
-        ))}
-        {["+", "-", "*", "/"].map((operator) => (
-          <button key={operator} onClick={() => handleInput(operator)}>
-            {operator}
-          </button>
-        ))}
-        <button onClick={handleCalculate}>=</button>
+    <div className="container my-4">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          <div className="input-group mb-3">
+            <input
+              id="input-conta"
+              type="text"
+              className="form-control"
+              placeholder="0"
+              value={input}
+              onChange={(evento) => setInput(evento.target.value)}
+              onKeyDown={handleKeyDown}
+              aria-label="Calculadora"
+              aria-describedby="basic-addon2"
+            />
+          </div>
+
+          {/* Linha para os botões */}
+          <div className="row mb-2">
+            <div className="col-3">
+              <button className="btn btn-danger w-100" onClick={handleClear}>
+                C
+              </button>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-secondary w-100" onClick={() => handleInput('(')}>
+                (
+              </button>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-secondary w-100" onClick={() => handleInput(')')}>
+                )
+              </button>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-info w-100" onClick={() => handleInput('log(')}>
+                log
+              </button>
+            </div>
+          </div>
+
+          {/* Linha com números e divisão */}
+          <div className="row mb-2">
+            {[7, 8, 9].map((num) => (
+              <div className="col-4" key={num}>
+                <button className="btn btn-light w-100" onClick={() => handleInput(num.toString())}>
+                  {num}
+                </button>
+              </div>
+            ))}
+            <div className="col-4">
+              <button className="btn btn-warning w-100" onClick={() => handleInput('/')}>
+                /
+              </button>
+            </div>
+          </div>
+
+          {/* Linha com mais números e multiplicação */}
+          <div className="row mb-2">
+            {[4, 5, 6].map((num) => (
+              <div className="col-4" key={num}>
+                <button className="btn btn-light w-100" onClick={() => handleInput(num.toString())}>
+                  {num}
+                </button>
+              </div>
+            ))}
+            <div className="col-4">
+              <button className="btn btn-warning w-100" onClick={() => handleInput('*')}>
+                *
+              </button>
+            </div>
+          </div>
+
+          {/* Linha com os últimos números e subtração */}
+          <div className="row mb-2">
+            {[1, 2, 3].map((num) => (
+              <div className="col-4" key={num}>
+                <button className="btn btn-light w-100" onClick={() => handleInput(num.toString())}>
+                  {num}
+                </button>
+              </div>
+            ))}
+            <div className="col-4">
+              <button className="btn btn-warning w-100" onClick={() => handleInput('-')}>
+                -
+              </button>
+            </div>
+          </div>
+
+          {/* Linha para o 0 e adição */}
+          <div className="row mb-2">
+            <div className="col-8">
+              <button className="btn btn-light w-100" onClick={() => handleInput('0')}>
+                0
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-warning w-100" onClick={() => handleInput('+')}>
+                +
+              </button>
+            </div>
+          </div>
+
+          {/* Linha com o botão de resultado */}
+          <div className="row">
+            <div className="col-12">
+              <button className="btn btn-success w-100" onClick={handleCalculate}>
+                =
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
